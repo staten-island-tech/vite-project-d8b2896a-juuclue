2b8616ea42000a24f0ce91893df152e3011d1cf8
juuclue <144699439+juuclue@users.noreply.github.com>
2023-11-22 09:54:02 -0500

    e

--- project/vite-project/js/main.js
+++ project/vite-project/js/main.js
@@ -3,9 +3,14 @@ import "../js/dom.js";
 import "../js/cards.js";
 import "../js/button.js";
 
-document.querySelector("#theme").addEventListener("click", function( {
-    if(document.body.classList.contains(light-pink)) {
-    document.body.classList.add(lavender)
-    document
+document.querySelector("#theme").addEventListener("click", function() {
+    if(document.body.classList.contains("pinkity")) {
+    document.body.classList.add("drinkity");
+    document.body.classList.remove("pinkity");
+    } else{
+    document.body.classList.add("pinkity");
+    document.body.classList.remove("driinkity");
     }
-}))
+});
+
+
